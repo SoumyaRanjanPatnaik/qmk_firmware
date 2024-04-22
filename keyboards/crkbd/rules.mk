a3726db49112a3aec9d9557d364c5f92f508ad36
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2024-04-22 11:21:01 +0530

    Update custom config

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -14,6 +14,9 @@ RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
 
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+
 # if firmware size over limit, try this option
 # LTO_ENABLE = yes
 
